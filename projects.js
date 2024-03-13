4e9b6383af79e2a1e290938724a41b13d68a5ce9
bryanleezh <leebryan307@gmail.com>
2024-03-13 19:47:41 +0100

    Feat: Add projects dummy data

---
+++ projects.js
@@ -0,0 +1,31 @@
+window.ProjectTypes = {
+    normal: "normal",
+    spicy: "spicy",
+}
+
+window.Projects = {
+    "s001": {
+        name: "Algorithm Visualizer",
+        description: "Pizza desc here",
+        type: ProjectTypes.normal,
+        src: "/images/characters/laptop_room.png", 
+        icon: "/images/characters/laptop_room.png",
+        actions: [ "saucyStatus", "clumsyStatus", "damage1" ],
+      },
+      "s002": {
+        name: "Python Platformer Game",
+        description: "A salty warrior who fears nothing",
+        type: ProjectTypes.normal,
+        src: "/images/characters/laptop_room.png",
+        icon: "/images/characters/laptop_room.png",
+        actions: [ "damage1", "saucyStatus", "clumsyStatus" ],
+      },
+    //   "v001": {
+    //     name: "Call Me Kale",
+    //     description: "Pizza desc here",
+    //     type: PizzaTypes.veggie,
+    //     src: "/images/characters/pizzas/v001.png",
+    //     icon: "/images/icons/veggie.png",
+    //     actions: [ "damage1" ],
+    //   },
+}
