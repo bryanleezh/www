821e5ecdb4fa5bad4aced929a35bb8abadffb58d
bryanleezh <leebryan307@gmail.com>
2024-04-23 04:04:12 +0200

    Add projects

--- projects.js
+++ projects.js
@@ -1,31 +1,55 @@
 window.ProjectTypes = {
     normal: "normal",
-    spicy: "spicy",
 }
 
 window.Projects = {
-    "s001": {
-        name: "Algorithm Visualizer",
-        description: "Pizza desc here",
-        type: ProjectTypes.normal,
-        src: "/images/characters/laptop_room.png", 
-        icon: "/images/characters/laptop_room.png",
-        actions: [ "saucyStatus", "clumsyStatus", "damage1" ],
-      },
-      "s002": {
-        name: "Python Platformer Game",
-        description: "A salty warrior who fears nothing",
-        type: ProjectTypes.normal,
-        src: "/images/characters/laptop_room.png",
-        icon: "/images/characters/laptop_room.png",
-        actions: [ "damage1", "saucyStatus", "clumsyStatus" ],
-      },
-    //   "v001": {
-    //     name: "Call Me Kale",
-    //     description: "Pizza desc here",
-    //     type: PizzaTypes.veggie,
-    //     src: "/images/characters/pizzas/v001.png",
-    //     icon: "/images/icons/veggie.png",
-    //     actions: [ "damage1" ],
-    //   },
+  "Template": {
+    name: "",
+    description: "",
+    src: "/images/projects/",
+    link: "",
+    techstack: "",
+  },
+  "MoneyPig": {
+    name: "Money Pig",
+    description: "Expenses Trips Tracker built in Next.js",
+    src: "/images/projects/money-pig.png",
+    link: "https://github.com/bryanleezh/money-pig",
+    techstack: "Next.js, TypeScript, FireBase",
+  },
+  "Access-Logger": {
+    name: "Ascendas Loyalty Access Logging App",
+    description: "Access logging application with multiple backend technologies that provides access control for different user roles",
+    src: "/images/projects/access-logger.png",
+    link: "https://github.com/bryanleezh/Ascendas-Loyalty-App",
+    techstack: "React.js, Express.js, SpringBoot, Flask, AWS",
+  },
+  "Portfolio-Manager": {
+    name: "Stocks Portfolio Manager",
+    description: "A Fully Managed Portoflio Analyzer in conjunction with Goldman Sachs with secure account management and permission control, along with real-time analysis of stock profiles",
+    src: "/images/projects/portfolio-analyzer.png",
+    link: "https://github.com/bryanleezh/goldman-sachs-portfolio-manager",
+    techstack: "Next.js, SpringBoot, MongoDB",
+  },
+  "Club-Management": {
+    name: "Club Management System",
+    description: "Centralised booking system for clubs with user authentication, scheduling and conflict management.",
+    src: "/images/projects/club-management.png",
+    link: "https://github.com/bryanleezh/club-management-system",
+    techstack: "React.js, Express.js, SpringBoot, Docker, Laravel"
+  },
+  "Algo-Visualizer": {
+    name: "Algorithm Visualizer",
+    description: "Interactive mario-themed visualiser allows users to understand more about algorithms through fun and interactive visualisations from start to end, along with the shortest paths",
+    src: "/images/projects/algo-visualiser.gif",
+    link: "https://algorithm-visualizer-tau-amber.vercel.app/",
+    techstack: "Vue.js"
+  },
+  "PyPlatformer": {
+    name: "Python Platformer Game",
+    description: "Platformer built in Pygame to understand Object-Oriented Programming",
+    src: "/images/projects/platformer.gif",
+    link: "https://bryanlee.itch.io/platformer",
+    techstack: "Python, PyGame",
+  },
 }
