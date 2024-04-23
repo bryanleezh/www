f2b27f39d9ae2fe67cb82f395dca2d8dfe5a9ada
bryanleezh <leebryan307@gmail.com>
2024-04-23 04:04:33 +0200

    Feat: Add pc to display projects

--- ProjectPc.js
+++ ProjectPc.js
@@ -1,25 +1,37 @@
+// PC that shows all projects with redirecting to Github
+// TODO: Add another pc class that would show individual projects that uses ProjectKeyboardMenu.js
+
 class ProjectPc extends GameObject {
     constructor(config) {
         super(config);
-        this.sprite = new this.Sprite({
+        this.sprite = new Sprite({
             gameObject: this,
             src: "/images/characters/projectpc.png",
             animations: {
                 "turned-on" : [ [0,0] ],
-                "turned-off": [ [1,0] ],
+                "turned-off": [ [2,0] ],
             },
-            currentAnimation: "turned-off"
+            currentAnimation: "turned-on"
         });
-        this.storyFlag = config.storyFlag;
-
+        // this.storyFlag = config.storyFlag;
+        // instantiate projects
+        this.projects = config.projects;
+        // can be placed here or extracted out to the main OverworldMap
         this.talking = [
-            
+            {
+                events: [
+                    { type: "textMessage", text: "Booting up the PC..." },
+                    { type: "projectMenu", projects: this.projects },
+                ]
+            }
         ];
     }
 
+    // TODO: Figure out how else i can turn on and off pc through state
     update() {
-        this.sprite.currentAnimation = playerState.storyFlag[this.storyFlag]
-        ? "turned-on"
-        : "turned-off"
+        // this.sprite.currentAnimation = "turned-off";
+        // this.sprite.currentAnimation = playerState.storyFlag[this.storyFlag]
+        // ? "turned-on"
+        // : "turned-off"
     }
 }
