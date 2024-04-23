588832621e5999499b1f723cd757040288ccc101
bryanleezh <leebryan307@gmail.com>
2024-04-23 21:08:55 +0200

    Feat: Add new indiv project menu

--- IndivProjectMenu.js
+++ IndivProjectMenu.js
@@ -11,28 +11,65 @@ class IndivProjectMenu {
     getOptions() {
         // TODO: Add info on img and logo
         const base = Projects[this.project];
-        return [{
-            label: base.name,
-            description: base.description,
-            handler: () => {
-                this.close();
+        // all options for the project
+        return [
+            {
+                label: base.name,
+                handler: () => {
+                    // this.close();
+                }
+            },
+            {
+                img: base.src,
+                handler: () => {
+                    // this.close();
+                }
+            },
+            {
+                description: base.description,
+                handler: () => {
+                    // this.close();
+                }
+            },
+            {
+                link: base.link,
+                handler: () => {
+                    this.redirect(base.link);
+                }
+            },
+            {
+                techstack: base.techstack,
+                handler: () => {
+                    // this.close();
+                }
+            },
+            {
+                label: "Close",
+                description: "Close the menu",
+                handler: () => {
+                    this.close();
+                }
             }
-        }]
+        ]
     }
 
     createElement() {
         this.element = document.createElement("div");
-        this.element.classList.add("ProjectMenu");
-        this.element.classList.add("overlayMenu");
-        // TODO: Edit header for project menu to be project name
+        this.element.classList.add("indivProjectMenu");
+
         const proj = Projects[this.project];
-        console.log(this.project);
+
         this.element.innerHTML = (`
             <h2>${proj.name}</h2>
         `)
     }
 
+    redirect(link) {
+        window.open(link, '_blank');
+    }
+
     close() {
+        this.esc?.unbind();
         this.keyboardMenu.end();
         this.element.remove();
         this.onComplete();
@@ -47,5 +84,12 @@ class IndivProjectMenu {
         this.keyboardMenu.setOptions(this.getOptions())
 
         container.appendChild(this.element);
+        
+        // bind escape key to leaving the project menu
+        // have a delay to prevent escape key from leaving the menu without it getting to open 1st
+        utilities.wait(200);
+        this.esc = new KeyPressListener("Escape" , () => {
+            this.close();
+        })
     }
 }
