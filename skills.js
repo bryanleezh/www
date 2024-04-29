3696936f7e2d21067f4e0d8b235e8cdac0ebce89
bryanleezh <leebryan307@gmail.com>
2024-04-29 23:44:08 +0200

    Add skills, frameworks & languages

--- skills.js
+++ skills.js
@@ -1,7 +1,105 @@
 // * All skills (languages, framework, tools)
 
 window.Skills = {
-    Languages: [],
-    Frameworks: [],
-    Tools: []
+    Languages: [
+        {
+            name: "Python",
+            description: "",
+            link: "https://www.python.org/",
+        },
+        {
+            name: "Java",
+            description: "",
+            link: "https://www.java.com/en/",
+        },
+        {
+            name: "Go",
+            description: "",
+            link: "https://go.dev/",
+        },
+        {
+            name: "Kotlin",
+            description: "",
+            link: "https://kotlinlang.org/",
+        },
+        {
+            name: "TypeScript",
+            description: "",
+            link: "https://www.typescriptlang.org/",
+        }
+    ],
+    Frameworks: [
+        {
+            name: "Next.js",
+            description: "",
+            link: "https://nextjs.org/",
+        },
+        {
+            name: "React.js",
+            description: "",
+            link: "https://react.dev/",
+        },
+        {
+            name: "Vue.js",
+            description: "",
+            link: "https://vuejs.org/",
+        },
+        {
+            name: "Node.js",
+            description: "",
+            link: "https://nodejs.org/en",
+        },
+        {
+            name: "SpringBoot",
+            description: "",
+            link: "https://spring.io/",
+        },
+        {
+            name: "Flask",
+            description: "",
+            link: "https://flask.palletsprojects.com/en/3.0.x/",
+        },
+        {
+            name: "Pandas",
+            description: "",
+            link: "https://pandas.pydata.org/",
+        },
+        {
+            name: "MongoDB",
+            description: "",
+            link: "https://www.mongodb.com/",
+        },
+        {
+            name: "Firebase",
+            description: "",
+            link: "https://firebase.google.com/",
+        },
+    ],
+    Tools: [
+        {
+            name: "AWS",
+            description: "",
+            link: "https://aws.amazon.com/",
+        },
+        {
+            name: "Docker",
+            description: "",
+            link: "https://www.docker.com/",
+        },
+        {
+            name: "Figma",
+            description: "",
+            link: "https://www.figma.com/",
+        },
+        {
+            name: "Jira",
+            description: "",
+            link: "https://www.atlassian.com/software/jira",
+        },
+        {
+            name: "Tableau",
+            description: "",
+            link: "https://www.tableau.com/",
+        },
+    ]
 }
